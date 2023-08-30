85114e392491bf2fb026cf036949b4dab0bfbeec
Carolina Coutinho <137925752+carolinamcoutinho@users.noreply.github.com>
2023-08-30 16:06:52 -0300

    Add files via upload

---
+++ fibo (1).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int N) 
+{ 
+  if (N <= 0) {
+        return 0;
+    } else if (N == 1) {
+        return 1;
+    } else {
+        return fibonacci(N - 1) + fibonacci(N - 2);
+    }
+    
+}
+
+int main(void) 
+{
+    int n, F;
+
+    cout << "Digite um número:" << endl;
+    cin >> n; 
+    
+    F=fibonacci(n);  
+
+    cout << "O termo " << n << " da série de Fibonacci é: " << F << endl;
+
+    return 0;
+}
