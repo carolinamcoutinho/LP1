e3dcb181c799986e2e285efcdfb2c9bae726c5bf
Carolina Coutinho <137925752+carolinamcoutinho@users.noreply.github.com>
2023-08-30 16:07:05 -0300

    Add files via upload

---
+++ mdc (1).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int mdc(int p, int q) 
+{ 
+  if (q == 0) {
+        return p;
+    } 
+  else {
+        return mdc(q, p % q);
+    }
+}
+    
+
+int main(void) 
+{
+    int p, q, divisor;
+
+    cout << "Digite dois números inteiros positivos:" << endl;
+    cin >> p >> q; 
+    
+    divisor=mdc(p,q);  
+
+    cout << "O MDC de (" << p <<", " << q << ") é: " << divisor << endl;
+
+    return 0;
+}
