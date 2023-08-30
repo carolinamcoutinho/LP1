24477058120e671a2ca66f712cf42641d524372f
Carolina Coutinho <137925752+carolinamcoutinho@users.noreply.github.com>
2023-08-30 16:06:20 -0300

    Add files via upload

---
+++ fatorial.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int fatorial(int N) 
+{ 
+  if (N == 0){
+    return 1;
+  }
+  else
+    return N*fatorial(N-1);
+    
+}
+
+int main(void) 
+{
+    int n, F;
+
+    cout << "Digite um número:" << endl;
+    cin >> n; 
+    
+    F = fatorial(n);
+
+    cout << "O fatorial de " << n << " é: " << F << endl;
+
+    return 0;
+}
